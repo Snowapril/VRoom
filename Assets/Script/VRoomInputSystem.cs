af229bbd133ee10e293df16336cfcf97364b9596
lusianm <47287650+lusianm@users.noreply.github.com>
2021-08-27 21:33:07 +0900

    feat(WebVR-InputSystem) : webXR Assembly Definition
    내가 구현하는 코드에서 WebXR NameSpace에 접근할 수 있도록, Script Folder에 Assembly Definition 파일 추가 및 WebXR을 참조하도록 설정

---
+++ Assets/Script/VRoomInputSystem.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebXR;
+
+public class VRoomInputSystem : MonoBehaviour
+{
+    private WebXRController rightHand, leftHand;
+    
+    private void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Update()
+    {
+        throw new NotImplementedException();
+    }
+}
+
